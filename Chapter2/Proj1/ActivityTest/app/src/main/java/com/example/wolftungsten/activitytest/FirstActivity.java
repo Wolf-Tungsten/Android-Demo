e77fc7500741049a2e9b0303d62b6dc9c4537917
Wolf Tungsten <gaoruihao@outlook.com>
2017-07-05 11:01:45 +0800

    Menu的使用

--- Chapter2/Proj1/ActivityTest/app/src/main/java/com/example/wolftungsten/activitytest/FirstActivity.java
+++ Chapter2/Proj1/ActivityTest/app/src/main/java/com/example/wolftungsten/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.example.wolftungsten.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,5 +21,26 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, "You clicked button1", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked remove",Toast.LENGTH_LONG).show();
+                break;
+            default:
+
+        }
+        return true;
     }
 }
